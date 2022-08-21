4cdf8011fc02c60751e8dac1e43fe8a3cd394335
Павел Матюсов <matyusovp@gmail.com>
2022-08-21 19:53:09 +0300

    не работабт миграции тут

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,8 +1,10 @@
 from django_filters.rest_framework import FilterSet, filters
 from rest_framework.filters import SearchFilter
+from django.contrib.auth import get_user_model
 
 from api.models import Recipe
 
+User = get_user_model()
 
 class IngredientFilter(SearchFilter):
     search_param = 'name'
@@ -17,14 +19,16 @@ class RecipeFilter(FilterSet):
 
     class Meta:
         model = Recipe
-        fields = ('tags', 'author', 'is_favorited', 'is_in_shopping_cart')
+        fields = ('tags', 'author',)
 
     def filter_is_favorited(self, queryset, name, value):
-        if value:
-            return queryset.filter(favorites__user=self.request.user)
+        user = self.request.user
+        if value and not user.is_anonymous:
+            return queryset.filter(favorites__user=user)
         return queryset
 
     def filter_is_in_shopping_cart(self, queryset, name, value):
-        if value:
-            return queryset.filter(shopping_carts__user=self.request.user)
+        user = self.request.user
+        if value and not user.is_anonymous:
+            return queryset.filter(shoppinglist__user=user)
         return queryset
