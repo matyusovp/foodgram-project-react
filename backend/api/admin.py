3aba276e482156503b50f788dd5a5600b46ab5aa
Павел Матюсов <matyusovp@gmail.com>
2022-08-21 22:36:09 +0300

    дамп базы+импорты

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,6 +1,8 @@
-from api.models import (Favorite, Ingredient, IngredientQnt, Recipe,
-                        ShoppingList, Tag)
 from django.contrib import admin
+
+from api.models import (Favorite, Ingredient, IngredientQnt, Recipe,
+
+                        ShoppingList, Tag)
 
 
 @admin.register(Tag)
