a1ff039be31db66a73a3d37ab2e6b42c9e53b546
Павел Матюсов <matyusovp@gmail.com>
2022-08-23 12:06:55 +0300

    правка админки

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,18 +1,17 @@
-from django.contrib.auth.admin import UserAdmin
 from django.contrib import admin
 
 from .models import Follow, UserProfile
 
 
 @admin.register(UserProfile)
-class UserAdmin(UserAdmin):
+class UserAdmin(admin.ModelAdmin):
     list_display = ('id', 'username', 'email', 'first_name', 'last_name')
     search_fields = ('username', 'email')
     list_filter = ('username', 'email')
 
 
 @admin.register(Follow)
-class FollowAdmin(UserAdmin):
+class FollowAdmin(admin.ModelAdmin):
     list_display = ('id', 'user', 'following')
     search_fields = ('user', 'following')
     list_filter = ('user', 'following')
