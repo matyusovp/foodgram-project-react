30705b39efaf41bd991c1778cca669a301ac33df
Павел Матюсов <matyusovp@gmail.com>
2022-08-24 12:54:37 +0300

    ингридиенты правка

--- backend/api/pagination.py
+++ backend/api/pagination.py
@@ -4,4 +4,4 @@ from rest_framework.pagination import PageNumberPagination
 class PageNumberPagination(PageNumberPagination):
     page_size = 6
     page_size_query_param = 'limit'
-    limit_query_param = 'recipes_limit'
+    # limit_query_param = 'recipes_limit'
